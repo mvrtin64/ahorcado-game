fa9da5a9c284627072dc3bb40e1e2501d093780a
mvrtin64 <mvrtingrzsk@gmail.com>
2023-10-25 21:53:48 -0300

    comentarios agregados a cada línea, corrección y eliminación de redundancia y código innecesario

--- public/script.js
+++ public/script.js
@@ -1,4 +1,4 @@
-// Configuración del cliente WebSocket
+// selecciono elementos del html para luego asignarlos a variables 
 const wordElement = document.querySelector('.word');
 const guessInput = document.querySelector('#letterInput');
 const guessButton = document.querySelector('#guessButton');
@@ -7,86 +7,83 @@ const resultContainer = document.querySelector('.result');
 const resultMessage = document.querySelector('#resultMessage');
 const resetButton = document.querySelector('#resetButton');
 
-let secretWord;
-let guessedWord = [];
-let intentosRestantes = 6;
+let secretWord;                      // declaro la variable para la palabra secreta
+let guessedWord = [];                           // palabra a adivinar declarada como arreglo vacío para reemplazarla primero con guiones bajos
+let intentosRestantes = 6;                       // declaro los intentos restantes para restarlos en caso de fallar
 
-const socket = new WebSocket('ws://localhost:3000');
+const socket = new WebSocket('ws://localhost:3000');              // conexión al servidor websocket en el puerto 3000
 
-socket.addEventListener('open', (event) => {
+socket.addEventListener('open', (event) => {                     // escuchador de eventos que avisa que se conectó exitosamente
   console.log('Conexión WebSocket establecida');
 });
 
 socket.addEventListener('message', (event) => {
-  console.log(event.data);
-  // Obtiene el elemento inicial de la palabra
-  const initialWordElement = document.querySelector('.initial-word');
+  /* console.log(event.data); */
+  const initialWordElement = document.querySelector('.initial-word');       // selecciono el html con el css de initial word y lo asigno a la variable 
 
-  const data = JSON.parse(event.data);
+  const data = JSON.parse(event.data);         // recibe el JSON y lo convierte a objeto, luego lo almacena en la variable 
 
   if (data.palabraSecreta) {
-    secretWord = data.palabraSecreta;
+    secretWord = data.palabraSecreta;                              // si data contiene la propiedad palabrasecreta, se almacena en la variable 
     
-    // Inicializa la palabra a adivinar con guiones bajos
-    initialWordElement.textContent = ' _'.repeat(secretWord.length);
-    // Inicializa guessedWord con guiones bajos para cada letra de la palabra secreta
+    initialWordElement.textContent = ' _'.repeat(secretWord.length);                          // inicializo la palabra secreta con guiones bajos 
     for (let i = 0; i < secretWord.length; i++) {
       guessedWord.push('_');
     }
   }
 
 
-  if (data.intentosRestantes !== undefined) {
-    attemptsLeft.textContent = data.intentosRestantes;
+  if (data.intentosRestantes !== undefined) {                  // verifico que el objeto data tenga la propiedad de objetos restantes
+    attemptsLeft.textContent = data.intentosRestantes;                // le asigno los intentos restantes a la variable 
   }
 
 });
 
-guessButton.addEventListener('click', () => {
-  const letter = guessInput.value.toLowerCase();
-  console.log('Letra adivinada:', letter); 
+guessButton.addEventListener('click', () => {                             // hago un escuchador de eventos a los clicks 
+  const letter = guessInput.value.toLowerCase();                                 // paso la letra a minúsculas
+  /* console.log('Letra adivinada:', letter);  */
 
   if (socket.readyState === WebSocket.OPEN) {
-    if (intentosRestantes > 0) {
-      const guessedLetter = puzzleProcessing(letter);
+    if (intentosRestantes > 0) {                               // si el estado del cliente es open, verifico la cantidad de intentos restantes
+      const guessedLetter = puzzleProcessing(letter);                   // mientras sea mayor a 0, procesa las letras que le voy pasando
 
-      if (letter.trim() === '') {
+      if (letter.trim() === '') {                                          // si no se escribe nada y aprieto el botón, no me resta intentos
         return;
       }
 
       if (!guessedLetter) {
-        intentosRestantes--;
-        attemptsLeft.textContent = intentosRestantes;
+        intentosRestantes--;                                        // si no adivino la letra, me va restando intentos
+        attemptsLeft.textContent = intentosRestantes;                        
       }
 
-      // Actualiza la palabra en la interfaz
-      wordElement.textContent = guessedWord.join(' ');
+      
+      wordElement.textContent = guessedWord.join(' ');                   // actualizo la palabra en la interfaz, junto todas las letras del arreglo
 
       if (intentosRestantes <= 0) {
         resultMessage.classList.add('lose-message');
-        resultMessage.textContent = '¡Perdiste! La palabra era: ' + secretWord;
+        resultMessage.textContent = '¡Perdiste! La palabra era: ' + secretWord;              // si se llega a 0 intentos restantes, pierde y muestra el mensaje con la palabra secreta
         resultMessage.className = 'lose';
-        guessInput.disabled = true; // Deshabilita el campo de entrada
-        guessButton.disabled = true; // Deshabilita el botón
+        guessInput.disabled = true;                                         // deshabilito el campo de entrada, ya que no hay más intentos restantes
+        guessButton.disabled = true;                                   // deshabilito el botón
       } else if (guessedWord.join('') === secretWord) {
-        resultMessage.textContent = '¡Ganaste! Has adivinado la palabra correctamente.';
-        resultMessage.className = 'win';
-        guessInput.disabled = true; // Deshabilita el campo de entrada
-        guessButton.disabled = true; // Deshabilita el botón
+        resultMessage.textContent = '¡Ganaste! Has adivinado la palabra correctamente.';                            // si al unir el arreglo, la palabra que quedó es la palabra secreta, sale el mensaje correspondiente
+        resultMessage.className = 'win';                        // distintos classnames para que tengan su respectivo color en css (verde y rojo)
+        guessInput.disabled = true;                                           // deshabilito de nuevo el campo y el botón, ya ganó
+        guessButton.disabled = true;                
       }
     }
   }
   /* console.log('Intentos restantes:', intentosRestantes);
   console.log('Palabra adivinada:', guessedWord.join(' ')); */
 
-  guessInput.value = ''; // Limpiar el campo de entrada después de adivinar
+  guessInput.value = '';                          // limpiar el campo de entrada después de enviar una letra
 });
 
 resetButton.addEventListener('click', () =>{                     // reiniciar la página al apretar el botón de reiniciar
   location.reload();
 });
 
-function puzzleProcessing(letter) {
+function puzzleProcessing(letter) {                            // si la letra está dentro de la palabra, reemplazar ese índice de la palabra por la letra y retornarla
   let guessedLetter = false;
   for (let i = 0; i < secretWord.length; i++) {
     if (secretWord[i] === letter) {
--- server.js
+++ server.js
@@ -1,61 +1,57 @@
-const express = require('express');
-const http = require('http');
+// importo módulo express, html, websocket path
+const express = require('express');               
+const http = require('http');                      
 const WebSocket = require('ws');
 const path = require('path');
 
-const app = express();
-const server = http.createServer(app);
-const wss = new WebSocket.Server({ server });
+const app = express();             // instancia de la aplicación Express
+const server = http.createServer(app);            // creo un servidor http a partir de la instancia
+const wss = new WebSocket.Server({ server });             // creo un server websocket utilizando el server http, para que se ejecuten en el mismo puerto
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));                   // configuración de Express para servir archivos estáticos desde la carpeta public en el directorio actual
 
-wss.on('connection', (ws) => {
+wss.on('connection', (ws) => {           // escucha el evento connection y ejecuta la lógica del servidor 
   /* console.log('Cliente conectado'); */
 
-  const palabrasParaAdivinar = ['manzana', 'perro', 'gato', 'casa', 'sol', 'mandarina', 'computadora', 'mate', 'jugo'];
+  const palabrasParaAdivinar = ['manzana', 'perro', 'gato', 'casa', 'sol', 'mandarina', 'computadora', 'mate', 'jugo'];      // lista de palabras para adivinar
 
   function seleccionarPalabraAleatoria() {
-    const indice = Math.floor(Math.random() * palabrasParaAdivinar.length);
+    const indice = Math.floor(Math.random() * palabrasParaAdivinar.length);        // genero un índice multiplicando un número random por la longitud de la lista y convirtiéndolo a entero 
     return palabrasParaAdivinar[indice];
   }
 
-  let palabraSecreta = seleccionarPalabraAleatoria();
+  let palabraSecreta = seleccionarPalabraAleatoria();            // selecciono aleatoriamente la palabra secreta
 
-  ws.send(JSON.stringify({ palabraSecreta: palabraSecreta }));
-  let palabraAdivinada = Array(palabraSecreta.length).fill('_');
-  let intentosFallidos = 0;
-  const maxIntentos = 6;
+  ws.send(JSON.stringify({ palabraSecreta: palabraSecreta }));                   // envío al cliente la palabra secreta en formato JSON, tiene un objeto con una propiedad que contiene el valor de la variable palabraSecreta
+  let palabraAdivinada = Array(palabraSecreta.length).fill('_');           // lleno la cantidad de letras de la palabra con guiones bajos, para adivinarlas
+  const maxIntentos = 6;                                                     // inicializo los intentos máximos en 6
 
-  function adivinarLetra(letra) {
+  function adivinarLetra(letra) {                
     let letraAdivinada = false;
-    for (let i = 0; i < palabraSecreta.length; i++) {
+    for (let i = 0; i < palabraSecreta.length; i++) {             // si la letra ingresada concuerda con una del índice de la palaabra secreta, se le asigna y la retorna
       if (palabraSecreta[i] === letra) {
         palabraAdivinada[i] = letra;
         letraAdivinada = true;
       }
     }
 
-    if (!letraAdivinada) {
-      intentosFallidos--;
-    }
-
     return letraAdivinada;
   }
 
-  ws.on('message', (message) => {
-    console.log(message);
-    if (message.length === 1) {
-      const data = JSON.parse(message);
-      const letra = data.letra.toLowerCase();
-      const letraAdivinada = adivinarLetra(letra);
+  ws.on('message', (message) => {                     // escucha el evento del mensaje recibido
+    /* console.log(message); */
+    if (message.length === 1) {               // verifico que se reciba una letra
+      const data = JSON.parse(message);            // analizo el mensaje como un objeto JSON para extraer información
+      const letra = data.letra.toLowerCase();                // paso el mensaje a minúsculas 
+      const letraAdivinada = adivinarLetra(letra);             // paso la letra a la función para adivinar letras 
 
       const estadoJuego = {
-        palabraAdivinada: palabraAdivinada.join(' '),
-        intentosRestantes: maxIntentos - intentosFallidos,
+        palabraAdivinada: palabraAdivinada.join(' '),                         // reemplazo los guiones bajos si ya fueron adivinados y separo mediante espacios
+        intentosRestantes: maxIntentos - intentosFallidos,                  // calculo los intentos restantes restando los intentos fallidos de los intentos máximos previamente declarados 
       };
 
-      wss.clients.forEach(client => {
-        if (client.readyState === WebSocket.OPEN) {
+      wss.clients.forEach(client => {                 // itera a través de cada cliente websocket del servidor 
+        if (client.readyState === WebSocket.OPEN) {                // si el estado del cliente es open, se envía el objeto estadojuego convertido en una cadena JSON 
           client.send(JSON.stringify(estadoJuego));
         }
       });
@@ -64,6 +60,6 @@ wss.on('connection', (ws) => {
 });
 
 
-server.listen(3000, () => {
+server.listen(3000, () => {                                            // hago que el servidor escuche en el puerto 3000
   console.log('Servidor en ejecución en http://localhost:3000');
 });
